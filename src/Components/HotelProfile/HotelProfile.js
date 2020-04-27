89cf4a4eb5dcce774bfc688c1029855949ec39ac
arturfilipd <arturfilipd@gmail.com>
2020-04-27 11:16:24 +0200

    :^)

--- src/Components/HotelProfile/HotelProfile.js
+++ src/Components/HotelProfile/HotelProfile.js
@@ -151,8 +151,8 @@ export default class HotelProfile extends Component{
         const dt2 = new Date(this.state.end);
         const diffDays = Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
 
-        const totalCost =  (diffDays * this.props.hotel.cost * this.state.rooms > 0)?(diffDays * this.props.hotel.cost * this.state.rooms ):"";
-        const button = (totalCost === "")?'smallButtonInactive':'smallButton';
+        const totalCost =  (diffDays * this.props.hotel.cost * this.state.rooms > 0 && new Date(this.state.start) >= new Date())?(diffDays * this.props.hotel.cost * this.state.rooms ):"--- ";
+        const button = (totalCost === "--- ")?'smallButtonInactive':'smallButton';
         const comments =this.state.comments.map(c => <Comment key={c.id} comment={c}/>)
         const favB = this.favoriteButton();
         return(
